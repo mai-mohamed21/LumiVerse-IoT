ac79e78ecd795cfda6e0d506f43688b22019fdf8
Mai Mohamed <167648460+mai-mohamed21@users.noreply.github.com>
2025-08-29 22:48:25 +0300

    Create SoundsModel class for sound page
    Define SoundsModel class with state fields for API call results.

---
+++ Flutter/lib/pages/sound/Model.dart
@@ -0,0 +1,31 @@
+import '/backend/api_requests/api_calls.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'sounds_widget.dart' show SoundsWidget;
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class SoundsModel extends FlutterFlowModel<SoundsWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // Stores action output result for [Backend Call - API (SOUND)] action in Container widget.
+  ApiCallResponse? apiResult6hc;
+  // Stores action output result for [Backend Call - API (SOUND)] action in Container widget.
+  ApiCallResponse? apiResultuzy;
+  // Stores action output result for [Backend Call - API (SOUND)] action in Container widget.
+  ApiCallResponse? apiResultvg2;
+  // Stores action output result for [Backend Call - API (SOUND)] action in Container widget.
+  ApiCallResponse? apiResultx2t;
+  // Stores action output result for [Backend Call - API (SOUND)] action in Container widget.
+  ApiCallResponse? apiResulthhl;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
