efd81c1686e69ae79623ad1cda495a374ac4d14c
Mai Mohamed <167648460+mai-mohamed21@users.noreply.github.com>
2025-08-29 22:45:46 +0300

    Create SensorsModel class for sensor data management

---
+++ Flutter/lib/pages/sensors/Model.dart
@@ -0,0 +1,27 @@
+import '/backend/supabase/supabase.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import '/index.dart';
+import 'sensors_widget.dart' show SensorsWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class SensorsModel extends FlutterFlowModel<SensorsWidget> {
+  ///  Local state fields for this page.
+
+  double? temp;
+
+  double? ldr;
+
+  double? vib;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
